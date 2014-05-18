e350875020e29e4ac81c29ccad8c5a000cdcb5fc
igakigongo <igakigongo@gmail.com>
2014-05-18 13:06:10 +0300

    Repeated setup of the project

---
+++ PManager.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using PManager.WebUI.Infrastructure.Abstract;
+using PManager.WebUI.Infrastructure.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PManager.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            this.kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.Get(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
