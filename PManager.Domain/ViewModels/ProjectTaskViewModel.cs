43ee32dad23d41a5ddc53e8787654d1da604026a
Paul Mugeere <paul@laboremus.no>
2014-07-14 09:16:16 +0300

    adding tasks

---
+++ PManager.Domain/ViewModels/ProjectTaskViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PManager.Domain.ViewModels
+{
+    public class ProjectTaskViewModel
+    {
+        public int ProjectId { get; set; }
+        public string TaskName { get; set; }
+        public Decimal Budget { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
